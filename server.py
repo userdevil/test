26ba020863bce3ed860fa4ace51cf786939ecc93
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-19 10:20:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import requests
 
 TELEGRAM_TOKEN = '5159256749:AAGXCe1j3yxC-x-i3yBKwznj_-bOIk7JyMw'
 
@@ -8,19 +9,24 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 def gen_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
-                               InlineKeyboardButton("No", callback_data="cb_no"))
+    markup.add(InlineKeyboardButton("MP4", callback_data="cb_yes"),
+                               InlineKeyboardButton("MP3", callback_data="cb_no"))
     return markup
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "cb_yes":
-        bot.answer_callback_query(call.id, "Answer is Yes")
+        bot.answer_callback_query(call.id, "Answer is MP4")
+        resp = "https://loader.to/api/button/?url=","message"
     elif call.data == "cb_no":
-        bot.answer_callback_query(call.id, "Answer is No")
+        bot.answer_callback_query(call.id, "Answer is MP3")
+        
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, resp)
 
 @bot.message_handler(func=lambda message: True)
 def message_handler(message):
-    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
+    bot.send_message(message.chat.id, "Select formet", reply_markup=gen_markup())
 
 bot.infinity_polling()
