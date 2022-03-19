729d69d45cbb05867a0c6a237088fe1163d7a7c3
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-19 10:42:04 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
 
 TELEGRAM_TOKEN = '5159256749:AAGXCe1j3yxC-x-i3yBKwznj_-bOIk7JyMw'
-
+msg = ""
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+    msg = message
     bot.reply_to(message, """\
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
@@ -20,7 +21,7 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-if message == hi:
+if msg == hi:
     @bot.message_handler(func=lambda message: True)
     def echo_message(message):
         r = "hello" 
