83f81735028c848c9fbad3a9a6c0ad3f7a1b4654
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-21 11:58:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
+    global stream
     try:
         youtub = pytube.YouTube(message)
         stream = youtub.streams.filter(file_extension='mp4').first().url
