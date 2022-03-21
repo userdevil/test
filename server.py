4412d20e4df7ca7afd07a196030184dbe3280de6
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-21 11:26:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -19,12 +19,12 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
-
-if msg == "hi":
-    @bot.message_handler(func=lambda message: True)
-    def echo_message(message):
-        r = "hello" 
-        bot.reply_to(message, r.text)
+    try:
+        yt = pytube.YouTube(message)
+        stream = yt.streams.filter(file_extension='mp4').first().url
+        tit = yt.title
+    except:
+        print("An exception occurred")
+    bot.reply_to(message, stream.text)
 
 bot.infinity_polling()
