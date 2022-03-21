425968212b916b7a1518c9481558836da2ba6c39
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-21 11:43:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,13 @@ TELEGRAM_TOKEN = '5159256749:AAGXCe1j3yxC-x-i3yBKwznj_-bOIk7JyMw'
 msg = ""
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+try:
+    yt = pytube.YouTube(message)
+    stream = yt.streams.filter(file_extension='mp4').first().url
+    tit = yt.title
+except:
+    print("An exception occurred")
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
@@ -19,13 +26,6 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    try:
-        yt = pytube.YouTube(message)
-        stream = yt.streams.filter(file_extension='mp4').first().url
-        tit = yt.title
-        global stream
-    except:
-        print("An exception occurred")
     bot.reply_to(message, stream.text)
 
 bot.infinity_polling()
