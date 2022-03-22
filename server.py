d5add61fecdaaf495753413674187cc218ef7846
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-22 18:10:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ quotes = [
 
 # loop through the quotes
 for quote in quotes:
-    url = 'https://api.telegram.org/bot5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs/sendMessage?chat_id=-784036670&text="{}"'.format(chat_id,quote)
+    url = 'https://api.telegram.org/bot5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs/sendMessage?chat_id=-784036670&text="{}"'.format(quote)
     requests.get(url)
     # sends new quotes every 20seconds
     time.sleep(30)
