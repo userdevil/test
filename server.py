ab2521d9e91a84dbb1579a4cde92de64daa630fd
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-22 17:50:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 import requests
 import time
+from telegram import*
+from datetime import datetime
+from telegram.ext import*
+
+bot = Bot("5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs")
+print(bot.get_me())
 
 # list of quotes
 quotes = [
@@ -9,10 +15,14 @@ quotes = [
     'https://www.youtube.com/watch?v=tYSrY4iPX6w',
     'https://www.youtube.com/watch?v=w7Fjxf62t8E'
 ]
+updater = Updater("5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs")
+updater = Updater("5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs",use_context = True)
+dispatcher = updater.
+CHAT_ID = update.effective_chat.id
 
 # loop through the quotes
 for quote in quotes:
-    url = 'https://api.telegram.org/bot5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs/sendMessage?chat_id=1224707304&text="{}"'.format(quote)
+    url = 'https://api.telegram.org/bot5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs/sendMessage?chat_id=&text="{}"'.format(quote)
     requests.get(url)
     # sends new quotes every 20seconds
     time.sleep(30)
