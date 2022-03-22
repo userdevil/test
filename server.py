f340775b068426cbb35d484cf32a33611ed6491a
userdevil <62639000+userdevil@users.noreply.github.com>
2022-03-22 17:29:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,39 +1,18 @@
-import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
-import pytube
-import re
-import shutil
+import time
 
-TELEGRAM_TOKEN = '5159256749:AAGXCe1j3yxC-x-i3yBKwznj_-bOIk7JyMw'
-stream = ""
-url = str("")
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+# list of quotes
+quotes = [
+    'https://www.youtube.com/watch?v=8FAUEv_E_xQ',
+    'https://www.youtube.com/watch?v=jmwU1iAC-IE',
+    'https://www.youtube.com/watch?v=ATElufr0OiE',
+    'https://www.youtube.com/watch?v=tYSrY4iPX6w',
+    'https://www.youtube.com/watch?v=w7Fjxf62t8E'
+]
 
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to help to download the youtube just send me the youtube video link\
-""")
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    global stream
-    try:
-        msg = message.text
-        youtub = pytube.YouTube(msg)
-        stream = youtub.streams.filter(file_extension='mp4').first().url
-        print(youtub.title)
-        bot.reply_to(message,youtub.title)
-        print(stream)
-        bot.reply_to(message,stream)
-    except:
-        print("An exception occurred")
-        bot.reply_to(message,"An exception occurred or send me a correct url if you continueslly getting this error try this app https://codingwithms-60edd.web.app/app.apk")
-
-bot.infinity_polling()
+# loop through the quotes
+for quote in quotes:
+    url = 'https://api.telegram.org/bot5245409735:AAH4IX_xxue3-lB1YTUEH1bxz1TqEFl9bFs/sendMessage?chat_id=CHAT_ID&text="{}"'.format(quote)
+    requests.get(url)
+    # sends new quotes every 20seconds
+    time.sleep(30)
